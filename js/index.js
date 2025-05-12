add5bc96e35dfaebe3f50a1d349ad9e35f58a3c8
dominique-thomas <52081569+dominique-thomas@users.noreply.github.com>
2025-05-12 09:13:55 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
+/**
+ * Project: Pre-Dinner Prep Simulation
+ * Description: JavaScript logic for embedding or interacting with Unity WebGL build and interface elements.
+ * Author: Dominique Thomas (github.com/dominique-thomas)
+ * License: Shared publicly for demonstration purposes only. Reuse or redistribution not permitted without permission.
+ */
 $(document).ready(function () {
     
     $("#helpModal").on("hidden.bs.modal", function () {
